71000688076bf96fa87149106db1e02231101930
Cloud User <centos@ip-172-31-10-253.ap-northeast-2.compute.internal>
2016-12-21 06:34:01 +0000

    add config sample file

---
+++ config/config.sample.php
@@ -0,0 +1,17 @@
+<?php
+define('DB_HOST', '');
+define('DB_USER', '');
+define('DB_PASS', '');
+define('DB_NAME', '');
+define('SITE_URL', 'http://localhost/');
+define('SITE_SHORT_URL', 'http://localhost/api/v1/createShortURL.php');
+define('SITE_TITLE', 'ShortURL');
+define('VERSION', '1.0.0');
+
+define('URL_PROTOCOLS', 'http|https|ftp|ftps|mailto|news|mms|rtmp|rtmpt|e2dk');
+
+define('KEY', ''); // 임의의 영문자 셋팅
+
+error_reporting(E_ALL);
+
+session_start();
